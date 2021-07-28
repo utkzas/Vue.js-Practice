4ceffe813c1f5474c6c9862bbc64ce344c019543
adfdsfd <46284927+utkzas@users.noreply.github.com>
2021-07-28 13:49:12 +0530

    Added Vuetify themes

--- todo-list/src/plugins/vuetify.js
+++ todo-list/src/plugins/vuetify.js
@@ -1,7 +1,15 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify,{
+    iconfont: 'md',
+    theme: {
+        primary : '#9652ff',
+        success : '#3cd1c2',
+        info: '#ffaa2c',
+        error: '#f83e70'
+    }
+});
 
 export default new Vuetify({
 });

4b0de8b0951392735b7ee788f2a14ff9ba5c2b98
adfdsfd <46284927+utkzas@users.noreply.github.com>
2021-07-28 13:45:44 +0530

    Toolbars and navigation drawer

---
+++ todo-list/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+                
+            <v-toolbar-title>
+                <span class="font-weight-light">
+                   Todo
+                </span>
+                <span>
+                    List
+                </span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn color="grey">
+                <span>Sign Out</span>
+                <v-icon right>mdi-logout</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <v-navigation-drawer v-model="drawer" app class="indigo">
+            <p>test</p>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            drawer:false
+        }
+    }
+}
+</script>
--- todo-list/src/views/Home.vue
+++ todo-list/src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
-  <hello-world />
+<div class="home">
+  <h1>Homepage</h1>
+</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
+  
   export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+    name: 'Home'
   }
 </script>
